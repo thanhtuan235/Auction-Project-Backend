50e650bdd833e30adbfd040608fb88bb3570cecd
thanhtuan235 <143677516+thanhtuan235@users.noreply.github.com>
2026-04-08 08:44:51 +0700

    feat: Complete GoogleAuth

--- auction_project/src/main/java/com/example/auction_project/controller/AuthController.java
+++ auction_project/src/main/java/com/example/auction_project/controller/AuthController.java
@@ -2,6 +2,7 @@ package com.example.auction_project.controller;
 
 import com.example.auction_project.dto.AuthRequest;
 import com.example.auction_project.dto.AuthResponse;
+import com.example.auction_project.dto.GoogleAuthRequest;
 import com.example.auction_project.dto.RegisterRequest;
 import com.example.auction_project.service.AuthenticationService;
 import lombok.RequiredArgsConstructor;
@@ -26,5 +27,10 @@ public class AuthController {
     @PostMapping("/login")
     public ResponseEntity<AuthResponse> authenticate(@RequestBody AuthRequest request) {
         return ResponseEntity.ok(service.authenticate(request));
+    }
+
+    @PostMapping("/google")
+    public ResponseEntity<AuthResponse> googleAuthenticate(@RequestBody GoogleAuthRequest request) throws Exception {
+        return ResponseEntity.ok(service.googleLogin(request));
     }
 }
--- auction_project/src/main/java/com/example/auction_project/service/AuthenticationService.java
+++ auction_project/src/main/java/com/example/auction_project/service/AuthenticationService.java
@@ -2,10 +2,19 @@ package com.example.auction_project.service;
 
 import com.example.auction_project.dto.AuthRequest;
 import com.example.auction_project.dto.AuthResponse;
+import com.example.auction_project.dto.GoogleAuthRequest;
 import com.example.auction_project.dto.RegisterRequest;
 import com.example.auction_project.entity.User;
 import com.example.auction_project.jwt.JwtService;
 import com.example.auction_project.repository.UserRepository;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.gson.GsonFactory;
+import org.springframework.beans.factory.annotation.Value;
+import java.util.Collections;
+import java.util.UUID;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -49,5 +58,42 @@ public class AuthenticationService {
         
         var jwtToken = jwtService.generateToken(user);
         return new AuthResponse(jwtToken);
+    }
+
+
+//     GoogleAuth
+    @Value("${app.google.client-id}")
+    private String googleClientId;
+
+    public AuthResponse googleLogin(GoogleAuthRequest request) throws Exception {
+        GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(new NetHttpTransport(), new GsonFactory())
+                .setAudience(Collections.singletonList(googleClientId))
+                .build();
+
+        GoogleIdToken idToken = verifier.verify(request.idToken());
+        if (idToken != null) {
+            GoogleIdToken.Payload payload = idToken.getPayload();
+
+            String email = payload.getEmail();
+            String defaultUsername = email.substring(0, email.indexOf("@")); 
+
+            var user = repository.findByEmail(email).orElse(null);
+
+            if (user == null) {
+                user = User.builder()
+                        .username(defaultUsername + "_" + UUID.randomUUID().toString().substring(0, 5)) 
+                        .email(email)
+                        .passwordHash(passwordEncoder.encode(UUID.randomUUID().toString())) 
+                        .role("BIDDER") 
+                        .build();
+                repository.save(user);
+            }
+
+            var jwtToken = jwtService.generateToken(user);
+            return new AuthResponse(jwtToken);
+            
+        } else {
+            throw new RuntimeException("Google Token không hợp lệ!");
+        }
     }
 }
