73dac4406ee9282364f4020b4322d539413be9a5
thanhtuan235 <143677516+thanhtuan235@users.noreply.github.com>
2026-04-11 14:10:42 +0700

    feat(redo): Complete Conversation & Message API

--- auction_project/src/main/java/com/example/auction_project/controller/ConversationController.java
+++ auction_project/src/main/java/com/example/auction_project/controller/ConversationController.java
@@ -14,7 +14,6 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 import com.example.auction_project.dto.Conversation.ConversationResponse;
